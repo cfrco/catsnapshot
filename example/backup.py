ecc5e7fb41c89c22d7b9cbe08d9145d65e7e59d1
CatCfrco <z82206.cat@gmail.com>
2013-08-11 22:11:19 +0800

    TODO and a better example

--- example/backup.py
+++ example/backup.py
@@ -5,5 +5,10 @@ import catsnapshot.snapschedule as snapsch
 
 sm = SnapManager.from_json("example/config.json")
 print sm.configs
+
+# take the first snapshot
+if sm.logs.count("") == 0 :
+    sm.snapshot(["node"])
+
 snapsch.schedule_task(sm)
 snapsch.schedule_loop()
